49863f8da2ec0ff10d193cbdfe66d808f87daea1
Ylan Rizo <119898716+YlanR@users.noreply.github.com>
2024-02-26 19:47:10 -0800

    Update arrancar-proyectos.bat

--- arrancar-proyectos.bat
+++ arrancar-proyectos.bat
@@ -1,5 +1,4 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe"
 start cmd.exe /k "cd C:\Users\Ylan\Desktop\api-red-social && npm start"
-@REM start cmd.exe /k "cd C:\Users\Ylan\Desktop\Programacion REACT\MasterReact\15-proyecto3 && npm run dev"
 start C:\Users\Ylan\AppData\Local\MongoDBCompass\MongoDBCompass.exe
 start C:\Users\Ylan\AppData\Local\Postman\Postman.exe
